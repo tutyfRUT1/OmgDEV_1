3b8100291f2621ca853ffdfb15e62c9878a00f1e
tutyfRUT1 <55320540+tutyfRUT1@users.noreply.github.com>
2020-03-23 19:29:03 -0300

    Add files via upload

---
+++ AstorWeb/AstorTilla-master/javascript_Inicial.js
@@ -0,0 +1,13 @@
+
+function Player(nombre,apellido,apodo,dorsal) {
+    this.Nombre = nombre
+    this.Apellido = apellido
+    this.Apodo = apodo
+    this.Dorsal = dorsal
+
+}
+
+let mJugador = new Player('Matias','De Olivera','Tuty',5)
+
+
+document.write(mJugador);
